d2808e3d8483f6d3f16e5d6c816b400cc2ecbf7b
OmarShamkh <omarshamkh2510@gmail.com>
2019-02-03 17:05:00 +0200

    Add solution for problem `A - Juicer` Link: https://codeforces.com/contest/709/problem/A Tags: implementation, *900 Ref: https://codeforces.com/contest/709/submission/49401691

--- codeforces/709/A.cpp
+++ codeforces/709/A.cpp
@@ -3,18 +3,23 @@
 using namespace std;
 int main(){
 
-int n , b ,d , a, sum=0 , c =0;
-cin>> n >> b >> d ; 
+int n , b ,d , a[100005], sum=0 , c =0;
+cin>> n >> b >> d ;
 for(int i=0;i<n;i++){
-   cin>>a;
-   if(a<=b){
-    sum+=a;
+   cin>>a[i];
+   if(a[i]<=b){
+    sum+=a[i];
     if(sum>d){
     c++;
     sum =0;
     }
+
    }
 }
 cout<<c;
+
+
+
+
     return 0;
 }
