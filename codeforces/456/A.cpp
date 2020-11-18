8a18267fa3e96b02fdfa7956d2d5f141fa0549f7
OmarShamkh <omarshamkh2510@gmail.com>
2020-11-18 14:16:00 +0200

    Add solution for problem `A - Laptops` Link: https://codeforces.com/contest/456/problem/A Tags: sortings, *1100 Ref: https://codeforces.com/contest/456/submission/98807896

--- codeforces/456/A.cpp
+++ codeforces/456/A.cpp
@@ -3,6 +3,16 @@
 #define sz(v)			((int)((v).size()))
 
 using namespace std;
+void print_map(map<int,int> mp)
+{
+    map <int, int> ::iterator itr;
+    for(itr =mp.begin(); itr != mp.end(); itr++)
+    {
+        cout  << itr->first << " " << itr->second << '\n';
+    }
+}
+
+map <int,int> mp;
 
 int main()
 {
@@ -12,11 +22,18 @@ int main()
     {
         int a, b;
         cin >> a >> b;
-        if( a != b){
-            cout << "Happy Alex";
+        mp[a] = b;
+    }
+    //print_map(mp);
+    map<int,int> ::iterator itr;
+    for (auto it= ++mp.begin(),itr= mp.begin(); it!=mp.end(); ++it,++itr)
+    {
+        if(itr->second > it->second  )
+        {
+            cout << "Happy Alex" ;
             return 0;
         }
     }
+
     cout << "Poor Alex" ;
-    return 0;
 }
