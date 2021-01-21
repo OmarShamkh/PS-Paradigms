d9fdf5fc269b2bba88f6a9beabe5fd72422efe40
OmarShamkh <omarshamkh2510@gmail.com>
2021-01-21 08:30:00 +0200

    Add solution for problem `C - Letters` Link: https://codeforces.com/contest/978/problem/C Tags: binary search, implementation, two pointers, *1000 Ref: https://codeforces.com/contest/978/submission/104957329

--- codeforces/978/C.cpp
+++ codeforces/978/C.cpp
@@ -1,32 +1,20 @@
 #include <bits/stdc++.h>
 using namespace std;
-typedef vector<int> vi;
 typedef long long ll;
-#define loop(n)          for(int i = 0; i < (n); i++)
-#define lp(x, s, e)      for(int x = (s); x < (e); x++)
-#define lpe(x, s, e)     for(int x = (s); x <= (e); x++)
-const int N = 2e5+10;
-ll n,m , a[N];
 int main()
 {
     cin.tie(0);
     cin.sync_with_stdio(0);
+    ll n,m;
     cin >> n >> m;
-    for(int i=0; i<n; i++){
-        cin >> a[i];
-        if(i) a[i] += a[i-1];
-    }
+    ll a[n],pre[n];
+    for(int i=0; i<n; i++) cin>>a[i];
+    for(int i=0; i<n; i++) pre[i] = a[i]+(i==0 ? 0 : pre[i-1]);
     for(int i=0; i<m; i++){
         ll x;
         cin >> x;
-        ll st = 0 , en = n-1 , mid;
-        while (st < en)
-        {
-            mid  = st + (en - st) / 2;
-            if(a[mid] >= x) en = mid;
-            else st = mid+1;
-        }
-        cout << st+1 << " "<< x - a[st-1] << "\n";
+        int idx = lower_bound(pre,pre+n,x)-pre;
+        cout << idx+1 <<" " << x-( (idx == 0 ? 0 : pre[idx-1]))<<"\n";
     }
     return 0;
 }
