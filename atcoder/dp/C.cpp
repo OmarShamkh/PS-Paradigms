5e8cf9b7f9be77882e430cff229a00302198e423
OmarShamkh <omarshamkh2510@gmail.com>
2021-04-03 12:56:00 +0200

    Add solution for problem `C - Vacation` Link: https://atcoder.jp/contests/dp/tasks/dp_c Tags: AtCoder, *100 Ref: https://atcoder.jp/contests/dp/submissions/21479730

--- atcoder/dp/C.cpp
+++ atcoder/dp/C.cpp
@@ -8,29 +8,22 @@ typedef long long ll;
 #define INGZzz  cin.tie(0);cin.sync_with_stdio(0);
 const int N = 1e5+10;
 int n, a[N], b[N], c[N] , dp[3][N];
-int solve(int idx, int prev){
-    if(idx >= n) return 0;
-
-    int &ret = dp[prev][idx];
-    if(~ret) return ret;
-
-    if(prev == 0){
-        ret = max(solve(idx+1 , 1)+ a[idx] , solve(idx+1 , 2) + a[idx]);
-    }
-    if(prev == 1){
-        ret = max(solve(idx+1 , 0)+ b[idx] , solve(idx+1 , 2) + b[idx]);
-    }
-    if(prev == 2){
-        ret = max(solve(idx+1 , 1)+ c[idx] , solve(idx+1 , 0) + c[idx]);
-    }
-    return ret;
-}
 int main()
 {
+    cin.tie(0);
+    cin.sync_with_stdio(0);
     INGZzz
-    memset(dp,-1,sizeof dp);
     cin >> n;
     for(int i=0; i<n; i++) cin >> a[i] >> b[i] >> c[i];
-    cout << max({solve(0,0) , solve(0,1) , solve(0,2)});
+    dp[0][0] = a[0];
+    dp[1][0] = b[0];
+    dp[2][0] = c[0];
+
+    for(int i=1; i<n; i++){
+        dp[0][i] = a[i] + max(dp[1][i-1], dp[2][i-1]);
+        dp[1][i] = b[i] + max(dp[0][i-1], dp[2][i-1]);
+        dp[2][i] = c[i] + max(dp[1][i-1], dp[0][i-1]);
+    }
+    cout << max({dp[0][n-1], dp[1][n-1] , dp[2][n-1]}) << "\n";
     return 0;
 }
